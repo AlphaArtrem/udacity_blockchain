093d11fea8e2d06f0ef077c7e295bf56a2c8360b
AlphaArtrem <yashawasthi01@gmail.com>
2020-04-25 20:16:15 +0530

    Completed coffeebase contract for p3_supply_chain

--- p3_suppy_chain/contracts/coffeecore/Ownable.sol
+++ p3_suppy_chain/contracts/coffeecore/Ownable.sol
@@ -19,7 +19,7 @@ contract Ownable {
     }
 
     /// Define a function modifier 'onlyOwner'
-    modifier onlyOwner() {
+    modifier onlyowner() {
         require(isOwner(), "Access Denied");
         _;
     }
@@ -30,13 +30,13 @@ contract Ownable {
     }
 
     /// Define a function to renounce ownerhip
-    function renounceOwnership() public onlyOwner {
+    function renounceOwnership() public onlyowner {
         emit TransferOwnership(origOwner, address(0));
         origOwner = address(0);
     }
 
     /// Define a public function to transfer ownership
-    function transferOwnership(address newOwner) public onlyOwner {
+    function transferOwnership(address newOwner) public onlyowner {
         _transferOwnership(newOwner);
     }
 

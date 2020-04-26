f2803b039c24bd0830d2a1f292c6f324594269ca
AlphaArtrem <yashawasthi01@gmail.com>
2020-04-26 16:01:41 +0530

    Completed all tests in p3

--- p3_suppy_chain/test/TestSupplychain.js
+++ p3_suppy_chain/test/TestSupplychain.js
@@ -263,13 +263,40 @@ contract('SupplyChain', function(accounts) {
 
     // 9th Test
     it("Testing smart contract function fetchItemBufferOne() that allows anyone to fetch item details from blockchain", async() => {
+        const supplyChain = await SupplyChain.deployed()
 
+        // Retrieve the just now saved item from blockchain by calling function fetchItem()
+        const resultBufferOne = await supplyChain.fetchItemBufferOne.call(upc)
+        
+        // Verify the result set:
+        assert.equal(resultBufferOne[0], sku, 'Error: Invalid item SKU')
+        assert.equal(resultBufferOne[1], upc, 'Error: Invalid item UPC')
+        assert.equal(resultBufferOne[2], consumerID, 'Error: Missing or Invalid ownerID')
+        assert.equal(resultBufferOne[3], originFarmerID, 'Error: Missing or Invalid originFarmerID')
+        assert.equal(resultBufferOne[4], originFarmName, 'Error: Missing or Invalid originFarmName')
+        assert.equal(resultBufferOne[5], originFarmInformation, 'Error: Missing or Invalid originFarmInformation')
+        assert.equal(resultBufferOne[6], originFarmLatitude, 'Error: Missing or Invalid originFarmLatitude')
+        assert.equal(resultBufferOne[7], originFarmLongitude, 'Error: Missing or Invalid originFarmLongitude')
         
     })
 
     // 10th Test
     it("Testing smart contract function fetchItemBufferTwo() that allows anyone to fetch item details from blockchain", async() => {
-    
+         const supplyChain = await SupplyChain.deployed()
+
+        // Retrieve the just now saved item from blockchain by calling function fetchItem()
+        const resultBufferTwo = await supplyChain.fetchItemBufferTwo.call(upc)
+        
+        // Verify the result set:
+        assert.equal(resultBufferTwo[0], sku, "Error: Invalid SKU")
+        assert.equal(resultBufferTwo[1], upc, "Error: Invalid UPC")
+        assert.equal(resultBufferTwo[2], productID, "Error: Invalid productID")
+        assert.equal(resultBufferTwo[3], productNotes, "Error: Invalid productNotes")
+        assert.equal(resultBufferTwo[4], productPrice, "Error: Invalid productPrice")
+        assert.equal(resultBufferTwo[5], itemState, "Error: Invalid itemState")
+        assert.equal(resultBufferTwo[6], distributorID, "Error: Invalid distributorID")
+        assert.equal(resultBufferTwo[7], retailerID, "Error: Invalid retailerID")
+        assert.equal(resultBufferTwo[8], consumerID, "Error: Invalid consumerID")
         
     })
 

2d08e7d4b1a0b18b832709c2e03e8c728b28d5bc
AlphaArtrem <yashawasthi01@gmail.com>
2020-05-06 17:08:08 +0530

    Completed ERC721Enumerable contract in p5

--- p5_real_estate_marketplace/eth-contracts/contracts/ERC721Mintable.sol
+++ p5_real_estate_marketplace/eth-contracts/contracts/ERC721Mintable.sol
@@ -408,7 +408,7 @@ contract ERC721Enumerable is ERC165, ERC721 {
      * @return uint256 token ID at the given index of the tokens list owned by the requested address
      */
     function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256) {
-        require(index < balanceOf(owner));
+        require(index < balanceOf(owner), "Index out of bounds");
         return _ownedTokens[owner][index];
     }
 
@@ -427,7 +427,7 @@ contract ERC721Enumerable is ERC165, ERC721 {
      * @return uint256 token ID at the given index of the tokens list
      */
     function tokenByIndex(uint256 index) public view returns (uint256) {
-        require(index < totalSupply());
+        require(index < totalSupply(), "Index out of bounds");
         return _allTokens[index];
     }
 
@@ -545,10 +545,13 @@ contract ERC721Enumerable is ERC165, ERC721 {
 }
 
 contract ERC721Metadata is ERC721Enumerable, usingOraclize {
-    
     // TODO: Create private vars for token _name, _symbol, and _baseTokenURI (string)
+    string private _name;
+    string private _symbol;
+    string private _baseTokenURI;
 
     // TODO: create private mapping of tokenId's to token uri's called '_tokenURIs'
+    mapping (uint256 => string) _tokenURIs;
 
     bytes4 private constant _INTERFACE_ID_ERC721_METADATA = 0x5b5e139f;
     /*
@@ -561,14 +564,28 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 
     constructor (string memory name, string memory symbol, string memory baseTokenURI) public {
         // TODO: set instance var values
+        _name = name;
+        _symbol = symbol;
+        _baseTokenURI = baseTokenURI;
 
         _registerInterface(_INTERFACE_ID_ERC721_METADATA);
     }
 
     // TODO: create external getter functions for name, symbol, and baseTokenURI
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
+
+    function baseTokenURI() external view returns (string memory) {
+        return _baseTokenURI;
+    }
 
     function tokenURI(uint256 tokenId) external view returns (string memory) {
-        require(_exists(tokenId));
+        require(_exists(tokenId), "Token doesn't exist");
         return _tokenURIs[tokenId];
     }
 
@@ -579,6 +596,13 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
     // TIP #2: you can also use uint2str() to convert a uint to a string
         // see https://github.com/oraclize/ethereum-api/blob/master/oraclizeAPI_0.5.sol for strConcat()
     // require the token exists before setting
+
+    function _setTokenURI(uint256 tokenId) internal {
+        require(_exists(tokenId), "Token doesn't exist");
+        string tokenIdStr = usingOraclize.uint2str(tokenId);
+        string tokenURI = usingOraclize.strConcat(_baseTokenURI, tokenIdStr);
+        _tokenURIs[tokenId] = tokenURI;
+    }
 
 }
 
